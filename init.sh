0155e98bf87d7868ef9b0034b61e339af43aa0e3
Robert G <robert.guenzler@student.htw-berlin.de>
2016-04-16 18:18:01 +0200

    major changes to init.sh
    * improve/update setup_iosevka
    * add nvim to setup_vim
    * improve/update setup_sublimetext
    * extend test
    * rewrite init.sh cli
    * added option to run single setup functions

--- init.sh
+++ init.sh
@@ -6,10 +6,9 @@ DOT_DIR="$HOME/.dotfiles"
 arch=`uname`
 you=`whoami`
 
-iosevka_version="1.2.0"
-golang_version="1.5.2"
+golang_version="1.6.1"
 
-function update_dotfiles
+function update_dotfiles()
 {
     # update dotfiles
     [ -d "$DOT_DIR/.git" ] && git --work-tree="$DOT_DIR" --git-dir="$DOT_DIR/.git" pull origin master
@@ -17,7 +16,7 @@ function update_dotfiles
     git submodule update --init --recursive
 }
 
-function setup_git
+function setup_git()
 {
     echo "[CONFIG] Git global configuration"
     # git-configs
@@ -26,7 +25,7 @@ function setup_git
     git config --global core.excludesfile ~/.gitignore_global
 }
 
-function setup_homebrew
+function setup_homebrew()
 {
     echo "[BREW] Install"
     eval "$DOT_DIR/install/brew/install"
@@ -37,35 +36,45 @@ function setup_homebrew
     # eval Brewfile
     echo "[BREW] Evaluate Brewfile"
     brew bundle
+    echo "[BREW] Finished setting up homebrew\n"
 }
 
-function setup_zsh
+function setup_zsh()
 {
     echo "[ZSH] Link .zshrc"
-    # bacause I made $DOT_DIR/zsh my ZDOTDIR
+    # because I made $DOT_DIR/zsh my ZDOTDIR
     ln -sfv "$DOT_DIR/zsh/zshrc" "$DOT_DIR/zsh/.zshrc"
 
     echo "[ZSH] Link .zshenv and .fasd"
     ln -sfv "$DOT_DIR/zsh/modules/fasd/fasd.rc" ~/.fasdrc
     ln -sfv "$DOT_DIR/zsh/zshenv" ~/.zshenv
+
+    # pull submodules
+    # echo "[ZSH] Pull zsh submodules"
+    # git submodule foreach git pull origin master 1>/dev/null
+    # git submodule update --init --recursive
+
+    echo "[ZSH] Finished setting up zsh\n"
 }
 
-function setup_config
+function setup_config()
 {
     echo "[CONFIG] Link .config directory into $HOME"
     # .config dir -> home
     ln -sfv "$DOT_DIR/config" ~/.config
+    echo "[CONFIG] Finished setting up .config dir\n"
 }
 
-function setup_gogitparser
+function setup_gogitparser()
 {
     echo "[ZSH] Build go-gitparser"
     # build go-gitparser
     cd "$DOT_DIR/zsh/modules/info-functions/go-gitparser"
     eval "go build"
+    echo "[ZSH] Built go-gitparser\n"
 }
 
-function setup_golang_linux
+function setup_golang_linux()
 {
     echo "[GOLANG] Download"
     cd /tmp
@@ -74,35 +83,55 @@ function setup_golang_linux
     tar -C /usr/local -xzf "go$golang_version.linux-amd64.tar.gz"
 }
 
-function setup_iosevka
+function setup_iosevka()
 {
     echo "[FONT] Downloading Iosevka $iosevka_version"
+    iosevka_version = "$(curl https://github.com/be5invis/iosevka/releases/latest -s | awk '{i=match($5, "/v"); print substr($5, i+2, 5)}')"
+    echo "[FONT] Most recent version of Iosevka: $iosevka_version"
+
     mkdir "/tmp/iosevka$iosevka_version" && cd "/tmp/iosevka$iosevka_version"
 
-    curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/iosevka-$iosevka_version.tar.bz2"
-    curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/iosevka-slab-$iosevka_version.tar.bz2"
+    curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/01.iosevka-$iosevka_version.zip"
+    curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/04.iosevka-slab-$iosevka_version.zip"
 
     echo "[FONT] Installing Iosevka"
     if [[ "$arch" = "Darwin" ]]; then
-    # # #
-        tar -C ~/Library/Fonts -xzf "iosevka-slab-$iosevka_version.tar.bz2"
-        tar -C ~/Library/Fonts -xzf "iosevka-$iosevka_version.tar.bz2"
-    # # #
+    ###
+        unzip -d /Library/Fonts/Iosevka "01.iosevka-$iosevka_version.zip"
+        unzip -d /Library/Fonts/Iosevka-Slab "04.iosevka-slab-$iosevka_version.zip"
+    ###
     elif [[ "$arch" = "Linux" ]]; then
-    # # #
-        tar -C /usr/local/share/fonts/truetype -xzf "iosevka-slab-$iosevka_version.tar.bz2"
-        tar -C /usr/local/share/fonts/truetype -xzf "iosevka-$iosevka_version.tar.bz2"
+    ###
+        unzip -d /usr/local/share/fonts/truetype/iosevka "01.iosevka-$iosevka_version.zip"
+        unzip -d /usr/local/share/fonts/truetype/iosevka-slab "04.iosevka-slab-$iosevka_version.zip"
+    ###
     fi
+
+    echo "[FONT] Installed Iosevka monospace font\n"
 }
 
-function setup_vim
+function setup_vim()
 {
     echo "[VIM] Setup"
     ln -sfv $DOT_DIR/vim $HOME/.vim
     ln -sfv $DOT_DIR/vim/vimrc $HOME/.vimrc
+    # make centralized folders
+    mkdir -pv $DOT_DIR/vim/backups
+    mkdir -pv $DOT_DIR/vim/swaps
+    mkdir -pv $DOT_DIR/vim/undo
+
+    echo "[VIM] Initial PlugUpdate"
+    $(which vim) +PlugUpdate +:q! +:q! &>/dev/null
+
+    if [[ "$arch" = "Darwin" ]]; then
+        echo "[VIM] Linking config for NeoVim"
+        ln -sfv $DOT_DIR/vim $DOT_DIR/config/nvim
+        ln -sfv $DOT_DIR/vim/vimrc $DOT_DIR/config/nvim/init.vim
+    fi
+    echo "[VIM] Finished setting up vim\n"
 }
 
-function setup_karabiner
+function setup_karabiner()
 {
     echo "[OSX] Karabiner mods"
     # include karabiner-configuration into $HOME/Library/Application Support/Karabiner/private.xml
@@ -121,7 +150,7 @@ EOF
     fi
 }
 
-function setup_ff_userchrome
+function setup_ff_userchrome()
 {
     echo "[OSX] Firefix userChrome mod"
     if [ ! -d "$HOME/Library/Application Support/Firefox/Profiles" ]; then
@@ -132,7 +161,7 @@ function setup_ff_userchrome
     fi
 }
 
-function setup_osx_system
+function setup_osx_system()
 {
     echo "[OSX] Set system defaults"
     zsh "$DOT_DIR/osx/defaults.sh"
@@ -140,44 +169,20 @@ function setup_osx_system
     zsh "$DOT_DIR/osx/dock.sh"
 }
 
-function setup_sublimetext
+function setup_sublimetext()
 {
     echo "[ST3] Move preferences into place"
     # ~/Library/Application Support/Sublime Text 3/Packages/User/
-    cp -v "$DOT_DIR/sublimetext/User/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-    cp -v "$DOT_DIR/sublimetext/User/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+    local rem=$(pwd)
+    cd $DOT_DIR/sublimetext/User/Projects
+    for v in *; do
+        b64 $v -D; rm -fv $v;
+    done
+    cd $rem
+    cp -v "$DOT_DIR/sublimetext/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 }
 
-function run
-{
-    echo "Hey, $you (on $arch)"
-
-    echo ">>> Update dotfile repository"
-    echo ">>> Load git submodules"
-    # update_dotfiles
-    # setup_iosevka
-    setup_zsh
-    setup_config
-    setup_git
-    setup_vim
-
-    if [[ "$arch" = "Darwin" ]]; then
-        echo ">>> Start OSX specific actions"
-        setup_homebrew
-        setup_ff_userchrome
-        setup_karabiner
-        setup_osx_system
-        setup_sublimetext
-    # # #
-    elif [[ "$arch" = "Linux" ]]; then
-        echo ">>> Start Linux specific actions"
-        # setup_golang_linux
-        # setup linuxbrew for getting devel tools?
-    fi
-    # setup_gogitparser
-}
-
-function test
+function test()
 {
     echo "Hey, $you (on $arch)"
     declare -i compl=0
@@ -191,6 +196,18 @@ function test
         print -P "Has %F{green}~/.zshenv%f" && compl+=1
     else
         print -P "%F{red}! no .zshenv %f"
+    fi
+    # vim?
+    if [ -f "$HOME/.vimrc" ]; then
+        print -P "Has %F{green}vimrc%f" && compl+=1
+    else
+        print -P "%F{red}! no $HOME/.vimrc %f"
+    fi
+    # OSX: nvim?
+    if [ "$arch" = "Darwin" ] && [ -f "$HOME/.config/nvim/init.vim" ]; then
+        print -P "Has %F{green}NeoVim - init.vim%f" && compl+=1
+    else
+        print -P "%F{red}! either no OSX or no init.vim for NeoVim%f"
     fi
     # check if config was symlinked
     if [ -d "$HOME/.config" ]; then
@@ -216,16 +233,10 @@ function test
         print -P "%F{red}! either no OSX or no homebrew %f"
     fi
     # OSX: font?
-    if [ "$arch" = "Darwin" ] && [ -f "$HOME/Library/Fonts/iosevka-regular.ttf" ]; then
+    if [ "$arch" = "Darwin" ] && [ -f "/Library/Fonts/Iosevka/iosevka-regular.ttf" ]; then
         print -P "Has %F{green}Iosevka%f" && compl+=1
     else
         print -P "%F{red}! no iosevka %f"
-    fi
-    # gitparser?
-    if [ ! -z "$($ZDOTDIR/modules/info-functions/go-gitparser/go-gitparser)" ]; then
-        print -P "%F{green}go-gitparser%f works" && compl+=1
-    else
-        print -P "%F{red}! can't evaluate go-gitparser %f"
     fi
     # karabiner config?
     if [ -f "$HOME/Library/Application Support/Karabiner/private.xml" ]; then
@@ -233,12 +244,88 @@ function test
     else
         print -P "%F{red}! no karabiner config%f"
     fi
+    # gitparser?
+    if [ ! -z "$($ZDOTDIR/modules/info-functions/go-gitparser/go-gitparser)" ]; then
+        print -P "%F{green}go-gitparser%f works" && compl+=1
+    else
+        print -P "%F{red}! can't evaluate go-gitparser %f"
+    fi
 
-    declare -i all=9
-    print -P "%B%K{blue}>>> $compl/$all - $(( ($compl/$all)*100 ))%% complete%k"
+    declare -i all=11
+    print -P "%B%K{blue}>>> $compl/$all complete%k" #- $(( ($compl/$all)*100 ))%% complete%k"
 }
 
-[ "$#" = 0 ] && print -P "Usage: init.sh [test] [all]"
-[ "$1" = "all" ] && run
-[ "$1" = "test" ] && test
-# add option to run individual commands
+function run()
+{
+    echo "Hi, $you (on $arch)"
+
+    echo ">>> Update dotfile repository"
+    echo ">>> Load git submodules"
+    update_dotfiles
+    setup_iosevka
+    setup_zsh
+    setup_config
+    setup_git
+    setup_vim
+
+    if [[ "$arch" = "Darwin" ]]; then
+        echo ">>> Start OSX specific actions"
+        setup_homebrew
+        setup_ff_userchrome
+        setup_karabiner
+        setup_osx_system
+        setup_sublimetext
+    # # #
+    elif [[ "$arch" = "Linux" ]]; then
+        echo ">>> Start Linux specific actions"
+        # setup_golang_linux
+        # setup linuxbrew for getting devel tools?
+    fi
+    setup_gogitparser
+}
+
+function action()
+{
+    case $1 in
+        "zsh")
+            setup_zsh ;;
+        "vim")
+            setup_vim ;;
+        "sublimetext")
+            setup_sublimetext ;;
+        "homebrew")
+            setup_homebrew ;;
+        "config")
+            setup_config ;;
+        "font")
+            setup_iosevka ;;
+        "osx-system")
+            setup_osx_system ;;
+        "karabiner")
+            setup_karabiner ;;
+        "firefox")
+            setup_ff_userchrome ;;
+        "git")
+            setup_git ;;
+        "go-gitparser")
+            setup_gogitparser ;;
+        *)
+            echo $usage ;;
+    esac
+}
+
+#
+# script control
+#
+
+usage="Usage: init.sh [test] [all] [do <action>]\n\n actions: git,homebrew,vim,zsh,config,karabiner,sublimetext,osx-system,go-gitparser,firefox,font\n"
+case $1 in
+    "all")
+        run ;;
+    "test")
+        test ;;
+    "do")
+        action $2 ;;
+    *)
+        echo $usage
+esac

93564743a86863a8160b6719c6b3d1810cd64e6d
Robert G <robert.guenzler@student.htw-berlin.de>
2016-04-16 18:11:14 +0200

    extend and improve sublime text module

--- sublimetext/st-pref-sync.sh
+++ sublimetext/st-pref-sync.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 ST3_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages"
 
-# rsync -Pavu --files-from=$DOT_DIR/sublimetext/st-pref-sync.conf $ST3_DIR $DOT_DIR/sublimetext/
-
 rsync -Pavuz --files-from=$DOT_DIR/sublimetext/st-pref-sync.conf "$ST3_DIR" $DOT_DIR/sublimetext/
+
+for f in $DOT_DIR/sublimetext/User/Projects/{*.sublime-workspace,*.sublime-project,*.json}; do
+    base64 -i $f -o  $DOT_DIR/sublimetext/User/Projects/$(echo $f | base64) && rm -vf $f
+done

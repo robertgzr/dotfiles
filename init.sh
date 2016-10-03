a633120ce9cca679c819649d1349f9f89dd03125
Robert G <robert.guenzler@student.htw-berlin.de>
2016-10-03 16:59:49 +0200

    rewrite the output/ui

--- init.sh
+++ init.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env zsh
 
-export DOT_DIR
 DOT_DIR="$HOME/.dotfiles"
+export DOT_DIR
 
-arch=`uname`
-you=`whoami`
+XDG_CONFIG_HOME="$HOME/Library/Application Support"
 
-golang_version="1.6.1"
+arch="$(uname)"
+you="$(whoami)"
+
+config_apps=( mpv livestreamer youtube-dl bitbar )
+
+function report_status()
+{
+    print -P "%F{cyan}[$current_func]%f $1"
+}
+function report_error()
+{
+    print -P "err: %F{red}$1%f"
+}
+function report_warning()
+{
+    print -P "warn: %F{yellow}$1%f"
+}
+function report_success()
+{
+    print -P "✔ %F{green}$1%f"
+}
 
 function update_dotfiles()
 {
@@ -18,7 +37,8 @@ function update_dotfiles()
 
 function setup_git()
 {
-    echo "[CONFIG] Git global configuration"
+    local current_func="git"
+    report_status "global configuration"
     # git-configs
     ln -sfv "$DOT_DIR/git/gitconfig" ~/.gitconfig
     ln -sfv "$DOT_DIR/git/gitignore_global" ~/.gitignore_global
@@ -27,74 +47,90 @@ function setup_git()
 
 function setup_homebrew()
 {
-    echo "[BREW] Install"
+    local current_func="brew"
+    report_status "install"
     eval "$DOT_DIR/install/brew/install"
-    echo "[BREW] Update"
+    report_status "update"
     brew update
-    echo "[BREW] Upgrade"
+    report_status "upgrade"
     brew upgrade
     # eval Brewfile
-    echo "[BREW] Evaluate Brewfile"
+    report_status "evaluate Brewfile"
     brew bundle
-    echo "[BREW] Finished setting up homebrew\n"
+    report_status "finished setting up homebrew\n"
 }
 
 function setup_zsh()
 {
-    echo "[ZSH] Link .zshrc"
+    status="zsh"
+    report_status $status "Link .zshrc"
     # because I made $DOT_DIR/zsh my ZDOTDIR
     ln -sfv "$DOT_DIR/zsh/zshrc" "$DOT_DIR/zsh/.zshrc"
 
-    echo "[ZSH] Link .zshenv and .fasd"
+    report_status $status "Link .zshenv and .fasd"
     ln -sfv "$DOT_DIR/zsh/modules/fasd/fasd.rc" ~/.fasdrc
     ln -sfv "$DOT_DIR/zsh/zshenv" ~/.zshenv
 
     # pull submodules
-    # echo "[ZSH] Pull zsh submodules"
+    # report_status $status "Pull zsh submodules"
     # git submodule foreach git pull origin master 1>/dev/null
     # git submodule update --init --recursive
 
-    echo "[ZSH] Finished setting up zsh\n"
+    report_status $status "Finished setting up zsh\n"
 }
 
 function setup_config()
 {
-    echo "[CONFIG] Link .config directory into $HOME"
-    # .config dir -> home
-    ln -sfv "$DOT_DIR/config" ~/.config
-    echo "[CONFIG] Finished setting up .config dir\n"
+    local current_func="config"
+    report_status "set up various application configs"
+    # create .config if neccessary
+    if [[ ! -d "$XDG_CONFIG_HOME" ]]; then
+        report_error "It appears XDG_CONFIG_HOME is not set..."
+        return
+    fi
+
+    for app in $config_apps; do
+        report_status "linking $app"
+        ln -sfv "$DOT_DIR/$app" "$XDG_CONFIG_HOME"
+    done
+
+    report_status "Finished setting up config dir\n"
 }
 
 function setup_gogitparser()
 {
-    echo "[ZSH] Build go-gitparser"
+    local current_func="zsh"
+    report_status "Build go-gitparser"
     # build go-gitparser
     cd "$DOT_DIR/zsh/modules/info-functions/go-gitparser"
     eval "go build"
-    echo "[ZSH] Built go-gitparser\n"
+    report_status "Built go-gitparser\n"
 }
 
 function setup_golang_linux()
 {
-    echo "[GOLANG] Download"
+    local current_func="go"
+    local golang_version="1.6.1"
+    report_status "Download"
     cd /tmp
     curl -O "https://storage.googleapis.com/golang/go$golang_version.linux-amd64.tar.gz"
-    echo "[GOLANG] Install"
+    report_status "Install"
     tar -C /usr/local -xzf "go$golang_version.linux-amd64.tar.gz"
 }
 
 function setup_iosevka()
 {
-    echo "[FONT] Downloading Iosevka $iosevka_version"
+    local current_func="font"
+    report_status "Downloading Iosevka $iosevka_version"
     iosevka_version = "$(curl https://github.com/be5invis/iosevka/releases/latest -s | awk '{i=match($5, "/v"); print substr($5, i+2, 5)}')"
-    echo "[FONT] Most recent version of Iosevka: $iosevka_version"
+    report_status "Most recent version of Iosevka: $iosevka_version"
 
     mkdir "/tmp/iosevka$iosevka_version" && cd "/tmp/iosevka$iosevka_version"
 
     curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/01.iosevka-$iosevka_version.zip"
     curl -O "https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/04.iosevka-slab-$iosevka_version.zip"
 
-    echo "[FONT] Installing Iosevka"
+    report_status "Installing Iosevka"
     if [[ "$arch" = "Darwin" ]]; then
     ###
         unzip -d /Library/Fonts/Iosevka "01.iosevka-$iosevka_version.zip"
@@ -107,12 +143,13 @@ function setup_iosevka()
     ###
     fi
 
-    echo "[FONT] Installed Iosevka monospace font\n"
+    report_status "Installed Iosevka monospace font\n"
 }
 
 function setup_vim()
 {
-    echo "[VIM] Setup"
+    local current_func="vim"
+    report_status "Setup"
     ln -sfv $DOT_DIR/vim $HOME/.vim
     ln -sfv $DOT_DIR/vim/vimrc $HOME/.vimrc
     # make centralized folders
@@ -120,20 +157,21 @@ function setup_vim()
     mkdir -pv $DOT_DIR/vim/swaps
     mkdir -pv $DOT_DIR/vim/undo
 
-    echo "[VIM] Initial PlugUpdate"
+    report_status "Initial PlugUpdate"
     $(which vim) +PlugUpdate +:q! +:q! &>/dev/null
 
     if [[ "$arch" = "Darwin" ]]; then
-        echo "[VIM] Linking config for NeoVim"
+        report_status "Linking config for NeoVim"
         ln -sfv $DOT_DIR/vim $DOT_DIR/config/nvim
         ln -sfv $DOT_DIR/vim/vimrc $DOT_DIR/config/nvim/init.vim
     fi
-    echo "[VIM] Finished setting up vim\n"
+    report_status "Finished setting up vim\n"
 }
 
 function setup_karabiner()
 {
-    echo "[OSX] Karabiner mods"
+    local current_func="osx"
+    report_status "Karabiner mods"
     # include karabiner-configuration into $HOME/Library/Application Support/Karabiner/private.xml
     [ ! -d "$HOME/Library/Application Support/Karabiner" ] && echo "Can't find Karabiner Config directory. Is Karabiner installed?"
         return 1
@@ -146,15 +184,16 @@ function setup_karabiner()
 </root>
 EOF
     else
-        echo "Has Karabiner config"
+        report_warning "There is a Karabiner config already"
     fi
 }
 
 function setup_ff_userchrome()
 {
-    echo "[OSX] Firefix userChrome mod"
+    local current_func="osx"
+    report_status "Firefix userChrome mod"
     if [ ! -d "$HOME/Library/Application Support/Firefox/Profiles" ]; then
-        echo "Can't find FF profile folder at ~/Library/Application Support/Firefox/Profiles/"
+        report_warning "Can't find FF profile folder at ~/Library/Application Support/Firefox/Profiles/"
         return 1
     else
         open -a Finder "$HOME/Library/Application Support/Firefox/Profiles"
@@ -163,15 +202,17 @@ function setup_ff_userchrome()
 
 function setup_osx_system()
 {
-    echo "[OSX] Set system defaults"
+    local current_func="osx"
+    report_status "Set system defaults"
     zsh "$DOT_DIR/osx/defaults.sh"
-    echo "[OSX] Configure dock"
+    report_status "Configure dock"
     zsh "$DOT_DIR/osx/dock.sh"
 }
 
 function setup_sublimetext()
 {
-    echo "[ST3] Move preferences into place"
+    local current_func="sublime"
+    report_status "Move preferences into place"
     # ~/Library/Application Support/Sublime Text 3/Packages/User/
     local rem=$(pwd)
     cd $DOT_DIR/sublimetext/User/Projects
@@ -186,81 +227,100 @@ function test()
 {
     echo "Hey, $you (on $arch)"
     declare -i compl=0
+    declare -i want=0
     # test zsh config
+    want+=1
     if [ -f "$ZDOTDIR/.zshrc" ]; then
-        print -P "Has %F{green}.zshrc%f" && compl+=1
+        report_success ".zshrc" && compl+=1
     else
-        print -P "%F{red}! no .zshrc"
+        report_error "missing .zshrc"
     fi
+    want+=1
     if [ -f "$HOME/.zshenv" ]; then
-        print -P "Has %F{green}~/.zshenv%f" && compl+=1
+        report_success ".zshenv" && compl+=1
     else
-        print -P "%F{red}! no .zshenv %f"
+        report_error "missing .zshenv"
     fi
     # vim?
+    want+=1
     if [ -f "$HOME/.vimrc" ]; then
-        print -P "Has %F{green}vimrc%f" && compl+=1
+        report_success "vimrc" && compl+=1
     else
-        print -P "%F{red}! no $HOME/.vimrc %f"
+        report_error "missing vimrc"
     fi
     # OSX: nvim?
+    want+=1
     if [ "$arch" = "Darwin" ] && [ -f "$HOME/.config/nvim/init.vim" ]; then
-        print -P "Has %F{green}NeoVim - init.vim%f" && compl+=1
+        report_success "init.vim (neovim)" && compl+=1
     else
-        print -P "%F{red}! either no OSX or no init.vim for NeoVim%f"
+        report_error "no init.vim for neovim"
     fi
     # check if config was symlinked
-    if [ -d "$HOME/.config" ]; then
-        print -P "Has %F{green}~/.config%f" && compl+=1
-    else
-        print -P "%F{red}! no ~/.config %f"
-    fi
+    for app in $config_apps; do
+        want+=1
+        if [ -d "$XDG_CONFIG_HOME/$app" ]; then
+            report_success $app && compl+=1
+        else
+            report_error "missing $app"
+        fi
+    done
+
     # test git config
+    want+=1
     if [ -f "$HOME/.gitconfig" ]; then
-        print -P "Has %F{green}.gitconfig%f" && compl+=1
+        report_success ".gitconfig" && compl+=1
     else
-        print -P "%F{red}! no .gitconfig %f"
+        report_error "missing .gitconfig"
     fi
+    want+=1
     if [ -f "$HOME/.gitignore_global" ]; then
-        print -P "Has %F{green}.gitignore_global%f" && compl+=1
+        report_success ".gitignore_global" && compl+=1
     else
-        print -P "%F{red}! no global gitignore %f"
+        report_error "missing global gitignore"
     fi
     # OSX: brew?
+    want+=1
     if [ "$arch" = "Darwin" ] && [ -f "/usr/local/bin/brew" ]; then
-        print -P "Has %F{green}Homebrew%f" && compl+=1
+        report_success "homebrew" && compl+=1
     else
-        print -P "%F{red}! either no OSX or no homebrew %f"
+        report_error "no OSX or missing homebrew"
     fi
     # OSX: font?
+    want+=1
     if [ "$arch" = "Darwin" ] && [ -f "/Library/Fonts/Iosevka/iosevka-regular.ttf" ]; then
-        print -P "Has %F{green}Iosevka%f" && compl+=1
+        report_success "Iosevka" && compl+=1
     else
-        print -P "%F{red}! no iosevka %f"
+        report_warning "missing Iosevka font"
     fi
     # karabiner config?
+    want+=1
     if [ -f "$HOME/Library/Application Support/Karabiner/private.xml" ]; then
-        print -P "Has %F{green}Karabiner%f custom keybindings" && compl+=1
+        report_success "Karabiner (custom keybindings)" && compl+=1
     else
-        print -P "%F{red}! no karabiner config%f"
+        report_warning "missingkarabiner config"
     fi
     # gitparser?
-    if [ ! -z "$($ZDOTDIR/modules/info-functions/go-gitparser/go-gitparser)" ]; then
-        print -P "%F{green}go-gitparser%f works" && compl+=1
+    want+=1
+    if [ ! -f "$ZDOTDIR/modules/info-function/go-gitparser/go-gitparser" ]; then
+        report_error "missing go-gitparser";
     else
-        print -P "%F{red}! can't evaluate go-gitparser %f"
+        if [ ! -z "$($ZDOTDIR/modules/info-functions/go-gitparser/go-gitparser)" ]; then
+            report_success "go-gitparser" && compl+=1;
+        else
+            report_warning "can't evaluate go-gitparser";
+        fi
     fi
 
-    declare -i all=11
-    print -P "%B%K{blue}>>> $compl/$all complete%k" #- $(( ($compl/$all)*100 ))%% complete%k"
+    print -P "%B%K{blue}>>> $compl/$want complete%k"
 }
 
 function run()
 {
+    local current_func="init"
     echo "Hi, $you (on $arch)"
 
-    echo ">>> Update dotfile repository"
-    echo ">>> Load git submodules"
+    report_status "Update dotfile repository"
+    report_status "Load git submodules"
     update_dotfiles
     setup_iosevka
     setup_zsh
@@ -269,15 +329,15 @@ function run()
     setup_vim
 
     if [[ "$arch" = "Darwin" ]]; then
-        echo ">>> Start OSX specific actions"
+        report_status "Start OSX specific actions"
         setup_homebrew
-        setup_ff_userchrome
+        # setup_ff_userchrome
         setup_karabiner
         setup_osx_system
         setup_sublimetext
     # # #
     elif [[ "$arch" = "Linux" ]]; then
-        echo ">>> Start Linux specific actions"
+        report_status "Start Linux specific actions"
         # setup_golang_linux
         # setup linuxbrew for getting devel tools?
     fi

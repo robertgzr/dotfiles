5ca119458fda74a39a45b0e246d7082ec8822165
Robert Günzler <robert.guenzler@student.htw-berlin.de>
2016-10-23 03:50:28 +0200

    move to ruby based init tool 'occupy'

---
+++ init.rb
@@ -0,0 +1,158 @@
+#!/bin/env ruby
+
+until File.exist?('./occupy.rb')
+  # get occupy gem
+end
+
+require './occupy.rb'
+
+# InitFramework.new takes 2 arguments
+# - you can change the dotdir location (default: ~/.dotfiles)
+# - and the config dir location (default: ~/.config)
+init = InitFramework.new
+
+# START of COMPONENTS
+
+InitModule.new('dotfiles') do |dot|
+  dot.shell('git submodule update --init --recursive')
+
+  init.register(dot)
+end
+
+InitModule.new('homebrew') do |brew|
+  brew.shell("eval #{init.dotdir}/install/brew/install")
+  brew.shell('brew update')
+  brew.shell('brew upgrade')
+  brew.shell('brew bundle')
+
+  init.register(brew)
+end
+
+InitModule.new('git') do |git|
+  git.links = [
+    InitLink.new(init, 'git/gitconfig', Dir.home + '/.gitconfig'),
+    InitLink.new(init, 'git/gitignore_global', Dir.home + '/.gitignore_global')
+  ]
+
+  git.shell('git config --global core.excludesfile $HOME/.gitignore_global')
+
+  init.register(git)
+end
+
+InitModule.new('vim') do |vim|
+  vim.needs_dir = [
+    init.dotdir + '/vim/backups',
+    init.dotdir + '/vim/swaps',
+    init.dotdir + '/vim/undo',
+    init.dotdir + '/vim/autoload/plug.vim'
+  ]
+  vim.links = [
+    InitLink.new(init, 'vim', Dir.home + '/.vim'),
+    InitLink.new(init, 'vim', init.configdir + '/nvim')
+  ]
+
+  # install vim-plug
+  vimplug_link = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  unless File.exist?("#{init.dotdir}/vim/autoload/plug.vim")
+    vim.shell("curl -fLo \"#{init.dotdir}/vim/autoload/plug.vim\" --create-dirs \"#{vimplug_link}\"")
+  end
+
+  # update vim-plug
+  vim.shell('$(which nvim) +PlugUpdate +:q! +:q! &>/dev/null')
+
+  init.register(vim)
+end
+
+InitModule.new('zsh') do |zsh|
+  zsh.links = [
+    InitLink.new(init, 'zsh/zshrc', init.dotdir + '/zsh/.zshrc'),
+    InitLink.new(init, 'zsh/modules/fasd.rc', Dir.home + '/.fasdrc'),
+    InitLink.new(init, 'zsh/zshenv', Dir.home + '/.zshenv')
+  ]
+
+  init.register(zsh)
+end
+
+InitModule.new('tmux') do |tmux|
+  tmux.links = [
+    InitLink.new(init, 'tmux/tmux.conf', Dir.home + '/.tmux.conf')
+  ]
+
+  init.register(tmux)
+end
+
+InitModule.new('config') do |config|
+  config.links = []
+
+  apps = ['mpv', 'livestreamer', 'youtube-dl', 'bitbar']
+  apps.each do |a|
+    config.links << InitLink.new(init, a, init.configdir + '/' + a)
+  end
+
+  init.register(config)
+end
+
+InitModule.new('osx') do |osx|
+  osx.shell("eval #{init.dotdir}/osx/defaults.sh")
+  osx.shell("eval #{init.dotdir}/osx/dock.sh")
+
+  init.register(osx)
+end
+
+InitModule.new('hammerspoon') do |hs|
+  hs.links = [
+    InitLink.new(init, 'hammerspoon', Dir.home + '/.hammerspoon')
+  ]
+
+  init.register(hs)
+end
+
+InitModule.new('porcelain') do |porc|
+  unless ENV.key?('GOPATH')
+    porc.report_error 'No Go environment found! Set GOPATH'
+  end
+
+  porc.shell('go get -u github.com/robertgzr/porcelain')
+  unless porc.shell('which porcelain')
+    porc.report_warning "Couldn't find 'porcelain'!\nIs GOPATH/bin in your PATH?"
+  end
+
+  init.register(porc)
+end
+
+InitModule.new('karabiner') do |kara|
+  karadir = Dir.home + '/Library/Application Support/Karabiner'
+  if Dir.exist?(karadir)
+    if File.exist?(karadir + 'private.xml')
+      kara.links = [
+        InitLink.new(init, 'osx/karabiner/private.xml', karadir)
+      ]
+    else
+      kara.report_warning 'There is a private.xml installed... Aborting.'
+    end
+  else
+    kara.report_warning 'Karabiner not installed?'
+  end
+
+  init.register(kara)
+end
+
+InitModule.new('sublimetext') do |subl|
+  subl.shell("for v in #{Dir.home}/sublimetext/User/Projects/*; do b64 \"$v\" -D; done")
+  subl.shell("cp \"#{init.dotdir}/sublimetext/User\" \"#{Dir.home}/Library/Application Support/Sublime Text 3/Packages/User\"")
+
+  init.register(subl)
+end
+
+# END of COMPONENTS
+
+init.dry_run if ARGV[0] == 'dry'
+init.run if ARGV[0] == 'run'
+init.test if ARGV[0] == 'test'
+puts %(Usage: ./init.rb [run | dry | test]
+
+run\t install everything
+dry\t show what it is going to install
+test\t test the system
+
+) if ARGV[0] == 'help'
---
+++ occupy.rb
@@ -0,0 +1,157 @@
+# TODO: Linking files is not working the way it should!
+
+# InitFramework wraps the components
+class InitFramework
+  attr_accessor :dotdir
+  attr_accessor :configdir
+  attr_accessor :modules
+
+  def initialize(
+    dot = Dir.home + '/.dotfiles',
+    config = Dir.home + '/.config'
+  )
+    @modules = []
+    @dotdir = dot
+    @configdir = config
+  end
+
+  def register(new_module)
+    raise 'Expected InitModule value' unless new_module.class == InitModule
+    @modules << new_module
+  end
+
+  def dry_run
+    puts 'Installing modules (dry run)'
+    @modules.each { |m| m.install(true) }
+  end
+
+  def run
+    puts 'Installing modules'
+    @modules.each(&:install)
+  end
+
+  def test
+    puts 'Running the tests'
+    @modules.each(&:test)
+  end
+end
+
+# InitLink wraps InitFramework links
+class InitLink
+  attr_accessor :src
+  attr_accessor :dst
+
+  require 'fileutils'
+
+  def initialize(framework, src, dst)
+    @fw = framework
+    @src = @fw.dotdir + '/' + src
+    @dst = dst
+  end
+
+  def link
+    FileUtils.ln_sf(src, dst)
+  end
+end
+
+# InitModule is a component of the InitFramework
+class InitModule
+  attr_accessor :name
+  attr_accessor :needs_dir
+  attr_accessor :links
+  attr_accessor :commands
+
+  def initialize(name)
+    raise 'Expected a name to be set' if name.empty?
+    @name = name
+    @commands = []
+
+    yield(self) if block_given?
+  end
+
+  def shell(cmd)
+    raise 'Expected a command' if cmd.nil?
+    @commands << cmd
+  end
+
+  # Links the objects in to_home to the users HOME dir
+  def link(dry = false)
+    @links.each do |l|
+      report "Linking: #{l.dst} -> #{l.src}"
+
+      l.link unless dry
+    end
+  end
+
+  def link_test
+    @links.each do |l|
+      check(l.dst)
+    end unless @links.nil?
+  end
+
+  # Creates the directories in needs_dir
+  def create_dirs(dry = false)
+    @needs_dir.each do |dir|
+      report "Creating: #{dir}"
+
+      begin
+        Dir.mkdir(dir) unless dry
+      rescue SystemCallError
+        report_warning 'directory exists!'
+      end
+    end
+  end
+
+  def create_dirs_test
+    @needs_dir.each { |d| check(d) } unless @needs_dir.nil?
+  end
+
+  def run_shell_commands(dry = false)
+    @commands.each do |cmd|
+      report "Running: #{cmd}"
+      Kernel.system(cmd) unless dry
+    end
+  end
+
+  def run_shell_commands_test
+    report_warning 'Can\' check if commands where successfull, sorry...'
+  end
+
+  def report(msg)
+    puts "\e[37m#{@name}:\e[0m #{msg}"
+  end
+
+  # prints in green
+  def report_success(msg)
+    puts "\e[37m#{@name}:\e[0m \e[32m✔ #{msg}\e[0m"
+  end
+
+  # prints in red
+  def report_error(msg)
+    puts "\e[37m#{@name}:\e[0m \e[31m✖ #{msg}\e[0m"
+  end
+
+  # prints in yellow
+  def report_warning(msg)
+    puts "\e[37m#{@name}:\e[0m \e[33m! #{msg}\e[0m"
+  end
+
+  def check(f)
+    File.exist?(f) ? report_success(f) : report_error("missing #{f}")
+  end
+
+  #
+  # main functions that get called by the framework
+  #
+  def install(dry = false)
+    create_dirs(dry) unless @needs_dir.nil?
+    link(dry) unless @links.nil?
+    run_shell_commands(dry) unless @commands.empty?
+  end
+
+  def test
+    create_dirs_test unless @needs_dir.nil?
+    link_test unless @links.nil?
+    run_shell_commands_test unless @commands.empty?
+  end
+end

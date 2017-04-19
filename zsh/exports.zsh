eab1e880a450f5def040c030ce4c90c68acb93de
Robert G <robertguenzler@gmail.com>
2017-04-19 17:42:19 +0200

    zsh: remove GREP_OPTIONS env

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -13,7 +13,6 @@ export CLICOLOR=1
 # export LSCOLORS=Gxfxcxdxbxegedabagacad
 
 # Enable color in grep
-export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions

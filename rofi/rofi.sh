b57c43642987e7b08b5bd8da9c6d505400f74d73
Robert Günzler <r@gnzler.io>
2019-02-28 19:43:44 +0100

    rofi: More default flags

--- rofi/rofi.sh
+++ rofi/rofi.sh
@@ -2,4 +2,12 @@
 
 set -ex
 
-exec /usr/bin/rofi -theme hibiki -font 'Iosevka 12' -drun-icon-theme Papirus -show-icons $@
+exec /usr/bin/rofi \
+    -theme hibiki \
+    -font 'Iosevka 12' \
+    -drun-icon-theme Papirus \
+    -show-icons \
+    -modi drun,run,combi,window \
+    -drun-show-actions \
+    -click-to-exit \
+    "${@}"

adffe196b0dd55d9056bc7cb1f1be715d8760dd1
Robert Günzler <r@gnzler.io>
2019-02-28 19:50:58 +0100

    zsh: Switch from xclip to wl-{copy,paste}

--- zsh/linux/aliases.zsh
+++ zsh/linux/aliases.zsh
@@ -1,7 +1,7 @@
-alias xclip='xclip -selection c'
-alias pbcopy='xclip -in'
+# alias xclip='xclip -selection c'
+alias pbcopy='wl-copy'
 alias pbc=pbcopy
-alias pbpaste='xclip -out'
+alias pbpaste='wl-paste'
 alias pbp=pbpaste
 
 #alias ssh='eval $(/usr/bin/keychain --eval --agents ssh -Q --quiet ~/.ssh/*_rsa) && ssh'
